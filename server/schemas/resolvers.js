1a048e9cd2f4c42212ad288ec1933ff1c5004ea5
Davidnino21 <davidnino_21@yahoo.com>
2024-02-29 16:44:42 -0800

    created schema types and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,64 @@
-const { Tech, Matchup } = require('../models');
+const { User } = require('../models')
+const { AuthenticationError, signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
-    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id })
+      }
+      throw AuthenticationError
+    }
   },
-  Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
-    },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
-    },
-  },
-};
 
-module.exports = resolvers;
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        throw AuthenticationError
+      }
+
+      const correctPw = await user.isCorrectPassword(password)
+
+      if (!correctPw) {
+        throw AuthenticationError
+      }
+
+      const token = signToken(user)
+      return { token, user }
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password })
+      const token = signToken(user)
+
+      return { token, user }
+    },
+    adoptDog: async (parent, { dogInput }, context) => {
+
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { adopted: dogInput } },
+          { new: true, runValidators: true }
+        );
+        return dogInput
+      }
+      throw AuthenticationError
+    },
+    // removeBook: async (parent, { bookId }, context) => {
+
+    //     if (context.user) {
+    //         const updatedUser = await User.findOneAndUpdate(
+    //             { _id: context.user._id },
+    //             { $pull: { savedBooks: { bookId: bookId } } },
+    //             { new: true}
+    //         );
+    //         return updatedUser
+    //     }
+    //     throw AuthenticationError
+    // },
+  }
+}
+
+module.exports = resolvers
