6189e5c6501f207ed0d1f8cc696943c855d557a2
Matthew_Hill <hillmatt58@gmail.com>
2024-03-03 13:45:35 -0700

    inital UI layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,45 +1,17 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_MATCHUPS } from '../utils/queries';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const Home = () => {
-  const { loading, data } = useQuery(QUERY_MATCHUPS, {
-    fetchPolicy: "no-cache"
-  });
 
-  const matchupList = data?.matchups || [];
+export default function Home() {
+    return (
+        <Container className='hero'>
+            <Col>
+              <img src='./src/assets/hero1.jpg' alt='Hero1' id='hero1' />
+              <Container className='background-container'></Container>
+              <img src='./src/assets/hero2.jpg' alt='Hero2' id='hero2' />
+            </Col>
 
-  return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
-      </div>
-      <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="square">
-            {matchupList.map((matchup) => {
-              return (
-                <li key={matchup._id}>
-                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                    {matchup.tech1} vs. {matchup.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
+        </Container>
+    );
+}
