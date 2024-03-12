9107260513e8d10d60abd16d779ea6a7999655f6
Matthew_Hill <hillmatt58@gmail.com>
2024-03-12 14:20:25 -0600

    deployment changes

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,31 +1,31 @@
 import Carousel from 'react-bootstrap/Carousel';
-import profile1 from '../assets/Profile1.png';
-import profile2 from '../assets/Profile2.png';
-import profile3 from '../assets/Profile3.png';
-import profile4 from '../assets/Profile4.png';
-import profile5 from '../assets/Profile5.png';
+import Profile1 from '../../public/assets/profile2.png';
+import Profile2 from '../../public/assets/profile2.png';
+import Profile3 from '../../public/assets/profile3.png';
+import Profile4 from '../../public/assets/profile4.png';
+import Profile5 from '../../public/assets/profile5.png';
 
 function CarouselFadeExample() {
   return (
     <Carousel fade>
       <Carousel.Item>
-        <img src={profile1} text="First slide" />
+        <img src={Profile1} text="First slide" />
 
       </Carousel.Item>
       <Carousel.Item>
-        <img src={profile2} text="Second slide" />
+        <img src={Profile2} text="Second slide" />
 
       </Carousel.Item>
       <Carousel.Item>
-        <img src={profile3} text="Third slide" />
+        <img src={Profile3} text="Third slide" />
 
       </Carousel.Item>
       <Carousel.Item>
-        <img src={profile4} text="Third slide" />
+        <img src={Profile4} text="Third slide" />
 
       </Carousel.Item>
       <Carousel.Item>
-        <img src={profile5} text="Third slide" />
+        <img src={Profile5} text="Third slide" />
 
       </Carousel.Item>
     </Carousel>
